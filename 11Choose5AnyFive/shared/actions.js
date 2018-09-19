5cf09867e82105c37c98ca8536e9b6091e75998a
Ting <ting@rytass.com>
2018-09-19 16:15:39 +0800

    base killer

--- 11Choose5AnyFive/shared/actions.js
+++ 11Choose5AnyFive/shared/actions.js
@@ -1,9 +1,11 @@
+import { baseKiller } from '../helper/baseKiller';
 import { sumKiller } from '../helper/sumKiller';
 import { acrossKiller } from '../helper/acrossKiller';
 import { tailKiller } from '../helper/tailKiller';
 import { allKiller } from '../helper/allKiller';
 
 export const FILTER_ACTIONS = [
+  baseKiller,
   sumKiller,
   acrossKiller,
   tailKiller,
