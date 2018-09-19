b8f6f898c44beff4fa55fdc370fea787f7399d91
Ting <ting@rytass.com>
2018-09-19 16:22:34 +0800

    type  keeper

--- 11Choose5AnyFive/shared/actions.js
+++ 11Choose5AnyFive/shared/actions.js
@@ -3,6 +3,14 @@ import { sumKiller } from '../helper/sumKiller';
 import { acrossKiller } from '../helper/acrossKiller';
 import { tailKiller } from '../helper/tailKiller';
 import { allKiller } from '../helper/allKiller';
+import {
+  bigTypeKeeper,
+  oddTypeKeeper,
+  primeTypeKeeper,
+  zeroLoadTypeKeeper,
+  oneLoadTypeKeeper,
+  twoLoadTypeKeeper,
+} from '../helper/typeKeeper';
 
 export const FILTER_ACTIONS = [
   baseKiller,
@@ -10,6 +18,12 @@ export const FILTER_ACTIONS = [
   acrossKiller,
   tailKiller,
   allKiller,
+  bigTypeKeeper,
+  oddTypeKeeper,
+  primeTypeKeeper,
+  zeroLoadTypeKeeper,
+  oneLoadTypeKeeper,
+  twoLoadTypeKeeper,
 ];
 
 export default {
