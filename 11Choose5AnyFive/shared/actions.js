55ad772d8563ede89559c88f2a51f5b7f0de4842
Ting <ting@rytass.com>
2018-09-19 16:02:32 +0800

    acrossKiller

--- 11Choose5AnyFive/shared/actions.js
+++ 11Choose5AnyFive/shared/actions.js
@@ -1,7 +1,9 @@
 import { sumKiller } from '../helper/sumKiller';
+import { acrossKiller } from '../helper/acrossKiller';
 
 export const FILTER_ACTIONS = [
   sumKiller,
+  acrossKiller,
 ];
 
 export default {
