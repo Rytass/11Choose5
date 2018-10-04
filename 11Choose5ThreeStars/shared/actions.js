33d65c0e5d9a92fd6ea991111124c79286c7d4b0
Ting <ting@rytass.com>
2018-10-04 19:01:44 +0800

    type killer

--- 11Choose5ThreeStars/shared/actions.js
+++ 11Choose5ThreeStars/shared/actions.js
@@ -2,12 +2,21 @@ import { baseKiller } from '../helper/baseKiller';
 import { chooseKiller } from '../helper/chooseKiller';
 import { acrossKiller } from '../helper/acrossKiller';
 import { allKiller } from '../helper/allKiller';
+import { sumKiller } from '../helper/sumKiller';
+
+import { bigSmallKiller } from '../helper/bigSmallKiller';
+import { oddEvenKiller } from '../helper/oddEvenKiller';
+import { primeCompositeKiller } from '../helper/primeCompositeKiller';
 
 export const FILTER_ACTIONS = [
   baseKiller,
   chooseKiller,
   acrossKiller,
   allKiller,
+  sumKiller,
+  bigSmallKiller,
+  oddEvenKiller,
+  primeCompositeKiller,
 ];
 
 export default {
