3d0d214c6be99f669899b9e06d5ef0db6d7a5121
Ting <ting@rytass.com>
2018-10-05 14:56:22 +0800

    choose and position killer

--- 11Choose5TwoStars/shared/actions.js
+++ 11Choose5TwoStars/shared/actions.js
@@ -1,7 +1,11 @@
 import { baseKiller } from '../helper/baseKiller';
+import { chooseKiller } from '../helper/chooseKiller';
+import { positionKiller } from '../helper/positionKiller';
 
 export const FILTER_ACTIONS = [
   baseKiller,
+  chooseKiller,
+  positionKiller,
 ];
 
 export default {
