44f31bb24a989dc644a002ef710d8cad8098ce20
Ting <ting@rytass.com>
2018-10-05 15:07:28 +0800

    sum and bile killer

--- 11Choose5TwoStars/shared/actions.js
+++ 11Choose5TwoStars/shared/actions.js
@@ -1,11 +1,16 @@
 import { baseKiller } from '../helper/baseKiller';
 import { chooseKiller } from '../helper/chooseKiller';
 import { positionKiller } from '../helper/positionKiller';
+import { bileKiller } from '../helper/bileKiller';
+
+import { sumKiller } from '../helper/sumKiller';
 
 export const FILTER_ACTIONS = [
   baseKiller,
   chooseKiller,
   positionKiller,
+  bileKiller,
+  sumKiller,
 ];
 
 export default {
